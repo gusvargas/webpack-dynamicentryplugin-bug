f9984e1929c3bff5179a02c09271846fb5273a41
Gus Vargas <gvargas@hubspot.com>
2019-03-05 14:35:15 -0500

    Repro

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // Function entry to cause DynamicEntryPlugin to be applied
+  entry: () => "foo",
+
+  plugins: [
+    {
+      apply(compiler) {
+        compiler.hooks.compilation.tap("UselessPlugin", compilation => {
+          compilation.hooks.seal.tap("UselessPlugin", () => {
+            throw new Error(
+              "I'm trapped in a Promise chain that won't handle me"
+            );
+          });
+        });
+      }
+    }
+  ]
+};
